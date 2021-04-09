a1f498ffd0c8ba4aa7897c185ad7b8cf58341e82
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-09 18:31:56 +0200

    add drop on click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log($); //test
+$(document).ready(function() {
+    /*********
+     * DROPDOWN MENU
+     *********/
+
+    // REFERENCES
+    var dropDown = $('.dropdown-list > li');
+    var dropMenu = dropDown.children('.dropdown-menu');
+
+    // Drop on Click
+    dropDown.click(function(){
+        var activeMenu = $(this).children('.dropdown-menu');
+
+        //reset
+        dropMenu.not(activeMenu).hide();
+
+        activeMenu.toggle();
+    })
+    //End Doc Ready
+});
