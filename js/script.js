ddb14f26f30ee91cfcb2ce6b256f93b769efef24
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-10 12:41:30 +0200

    add click everywhere fro hide

--- js/script.js
+++ js/script.js
@@ -7,14 +7,24 @@ $(document).ready(function() {
     var dropDown = $('.dropdown-list > li');
     var dropMenu = dropDown.children('.dropdown-menu');
 
+    dropMenu.hide(); //For hide display flex features menu
+
     // Drop on Click
     dropDown.click(function(){
         var activeMenu = $(this).children('.dropdown-menu');
-
+        
         //reset
         dropMenu.not(activeMenu).hide();
-
+        
         activeMenu.toggle();
+    })
+
+    //Click everywhere for hide
+    $(document).click(function(e) {
+        console.log(e);
+        if(!$(e.target).is(dropDown)){
+            dropMenu.hide(); //For hide display flex features menu
+        }
     })
     //End Doc Ready
 });
